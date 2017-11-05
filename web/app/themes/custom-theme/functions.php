6f7b849f2abcccebf5d91711f713b8f84166c5cc
Joseph Marshall <josephmarshall@Josephs-MacBook.local>
2017-11-05 12:16:38 +0000

    functions changed to add bootstrap

--- web/app/themes/custom-theme/functions.php
+++ web/app/themes/custom-theme/functions.php
@@ -53,5 +53,42 @@ function react_script() {
 }
 add_action('wp_enqueue_scripts', 'bootstrap_script');
 
+// Custom Article Post
+
+add_action( 'init', 'create_article_post_type' );
+
+function create_article_post_type()
+{
+// Articles post type
+register_post_type('article',
+        array(
+            'labels' => array(
+                'name' => __('Articles'),
+                'singular_name' => __('Article')
+            ),
+            'public' => true,
+            'description' => 'Neurology case study articles',
+            'menu_position' => 4,
+            'menu_icon' => 'dashicons-media-document',
+            'show_in_rest' => true,
+            'supports' => [
+                'thumbnail',
+                'title',
+//            'editor',
+                'author',
+//                'comments',
+                'revisions'
+
+            ],
+            'taxonomies' => [
+                'category',
+                'post_tag'
+            ],
+            'hierarchical' => false,
+            'has_archive' => false
+        )
+    );
+}
+
 
 ?>
